e862d0cdc799baeb730c9f947669a7c07f20f325
Jason Chao <jason.chao@infrontconsulting.com.my>
2021-09-05 20:00:39 +0800

    Added recorder.js. included localization of Malay and Chinese. Recorder no sound, Button css need fix, Localization is in Indonesia

--- subjects/views.py
+++ subjects/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from django.utils.translation import gettext as _
 # Create your views here.
 def consent(request):
     return render(request, 'consent-pop-up.html')
 
 def record(request):
+    if request.is_ajax():
+        audio = request.POST.get('fd')
+        print(audio)
     return render(request, 'record.html')
