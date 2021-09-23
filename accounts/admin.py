5157d272bbb72e9aaaac2e286dd1ce9f7ff1b4ac
Daniel Lim Zheng Yu <daniellim0510@Daniel-Lim-MacAir.local>
2021-09-23 11:40:25 +0800

    update db

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,3 @@
-from subjects.models import questionnairedata
 from django.contrib import admin
-#from .models import *
-from subjects.forms import questionnairedata
-from subjects.models import questionnairedata
 
 # Register your models here.
-@admin.register(questionnairedata)
-class questionnaireadmin(admin.ModelAdmin):
-    list_display = ['respondent_choices', 'respondent_sex', 'age', 'med_cond_opt', 'respondent_smoke', 'symptoms_opt', 'date_diagnosed']
