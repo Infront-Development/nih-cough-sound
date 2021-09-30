157e760fc8060677abb0765dd0bac18d6ea5a4da
Daniel Lim Zheng Yu <daniellim0510@Daniel-Lim-MacAir.local>
2021-09-30 15:48:16 +0800

    login

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import models
 from accounts.models import Subjects
+from django.forms import ValidationError
 
 class registerSubjectsForm(forms.ModelForm):
     subjects_phone_number = forms.CharField(label="Please Input your Phone Number")
@@ -11,8 +12,14 @@ class registerSubjectsForm(forms.ModelForm):
 
 class loginSubjectsForm(forms.ModelForm):
     subjects_login = forms.CharField(label="User ID ")
-    subjects_phone_number = forms.CharField(label="Phone Number :")
 
     class Meta:
         model = Subjects
-        fields = ('subjects_login','subjects_phone_number',)
+        fields = ('subjects_login',)
+
+    def clean_subject_login(self):
+        data  = self.cleaned_data
+        subject_login = data["subject_login"]
+        if not Subjects.objects.filter(subjects_login=subject_login).exists():
+            raise ValidationError("ID DOES NOT EXISTS")
+        return subject_login
--- accounts/views.py
+++ accounts/views.py
@@ -40,24 +40,33 @@ def identifier(request):
     if request.method == 'POST':
         form1 = registerSubjectsForm(request.POST)
         form2 = loginSubjectsForm(data=request.POST)
-        if form1.is_valid():
-            #commit false the form first
-            subjectsDetails = form1.save(commit=False)
-            #create identifier
-            letters = string.ascii_lowercase
-            frontText = "".join( [random.choice(letters) for i in range(1)] )
-            middleTwoNumber = randrange(99)
-            pNum = subjectsDetails.subjects_phone_number
-            last4digit = pNum[-4:]
-            id = frontText + "-" + str(middleTwoNumber) + str(last4digit)
-            print("id is here:  ",id)
-            # form1.save()
-            subjectsDetails.subjects_login = id
-            subjectsDetails.save()
-            print("subject is successfully created")
+        # if form1.is_valid():
+        #     #commit false the form first
+        #     subjectsDetails = form1.save(commit=False)
+        #     #create identifier
+        #     letters = string.ascii_lowercase
+        #     frontText = "".join( [random.choice(letters) for i in range(1)] )
+        #     middleTwoNumber = randrange(99)
+        #     pNum = subjectsDetails.subjects_phone_number
+        #     last4digit = pNum[-4:]
+        #     id = frontText + "-" + str(middleTwoNumber) + str(last4digit)
+        #     print("id is here:  ",id)
+        #     # form1.save()
+        #     subjectsDetails.subjects_login = id
+        #     subjectsDetails.save()
+        #     print("subject is successfully created")
+        #     return redirect('consent')
+        if form2.is_valid():
+            #i wan to check the existing subject with id
+            #if the user is existed
+            #redirect the subject to the coughing page
+            #else if not exited
+            #reinput the id again or the subject himself can go register
+            print("im here")
             return redirect('consent')
-        elif form2.is_valid():
-            return redirect('consent')
+        else:
+            print("im in else statement")
+            return render(request,"id_form.html",{'form1':form1,'form2': form2})
     else:
         form1 = registerSubjectsForm()
         form2 = loginSubjectsForm()
