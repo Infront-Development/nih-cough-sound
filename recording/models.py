829ab5b286dfb1a8b61654c6738ac28e33115215
Daniel Lim Zheng Yu <daniellim0510@Daniel-Lim-MacAir.local>
2021-10-01 15:23:19 +0800

    Recording Cough and Breath Migrations

--- recording/models.py
+++ recording/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from accounts.models import Subjects
 
 # Create your models here.
 def upload_to(instance, filename):
@@ -9,3 +10,9 @@ def upload_to(instance, filename):
 class Cough(models.Model):
     cough_id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False,unique=True)
     cough_record = models.FileField(upload_to=upload_to)
+    subjects = models.ForeignKey(Subjects,on_delete=models.CASCADE)
+
+class Breath(models.Model):
+    breath_id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False,unique=True)
+    breath_record = models.FileField(upload_to=upload_to)
+    subjects = models.ForeignKey(Subjects,on_delete=models.CASCADE)
