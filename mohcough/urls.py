9ec6866de87f6978f97825177dbda987cec36030
Jivha Raymond L <vharaymonten@gmail.com>
2021-11-05 15:37:55 +0700

    new tranlsation

--- mohcough/urls.py
+++ mohcough/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.i18n import JavaScriptCatalog
+from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
    
     path('', include('accounts.urls')),
     path('common/', include('common.urls', namespace="common")),
     path('i18n/', include('django.conf.urls.i18n')),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path('questionnaire/', include('questionnaire.urls', namespace="questionnaire")),
     path('recording/',include('recording.urls', namespace='recording')),
     
