198b74938d2cd75600b133fb3052b79c834cdf09
jiahern <jiahern@student.unimelb.edu.au>
2022-01-07 15:44:28 +0800

    4 main Recording Template UI Done

--- recording/urls.py
+++ recording/urls.py
@@ -7,7 +7,10 @@ app_name="recording"
 urlpatterns = [
     path('cough', views.cough_page, name="cough_page"),
     path('cough-no-mask', views.cough_no_mask_page, name="cough_no_mask_page"),
+    path('cough-with-mask', views.cough_with_mask_page, name="cough_with_mask_page"),
     # path('viewRecording', views.view_cough_recording, name="viewRecording"),
     path('breath',views.breath_page, name="breath_page"),
+    path('breath-with-mask', views.breath_with_mask_page, name="breath_with_mask_page"),
+    path('breath-no-mask', views.breath_no_mask_page, name="breath_no_mask_page"),
     path('instruc', views.instruc_page, name="instruc_page")
 ]
