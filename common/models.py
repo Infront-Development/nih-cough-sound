6012f18568f1285814c49caa705a79ee130f3302
Jivha Raymond L <vharaymonten@gmail.com>
2022-02-07 12:54:22 +0700

    feedback form complete

--- common/models.py
+++ common/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator, MaxValueValidator
 from accounts.models import Subject
 import uuid
 from django.utils.safestring import mark_safe
@@ -13,17 +14,13 @@ rating_choice = [
     ("5", mark_safe('<span class="fa fa-star checked mr-1 text-left" style="color:orange"></span>'*5)),
 ]
 
-
 class Feedback(models.Model): 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE,null=True)
-    rating = models.CharField(max_length=200 ,default= None,choices =rating_choice,verbose_name=_(mark_safe('<span class="font-weight-bold text-dark">Rating:</span>')))
+    rating = models.PositiveSmallIntegerField(max_length=200 ,default=5,
+                validators=[MinValueValidator(1), MaxValueValidator(5)],
+                verbose_name=_(mark_safe('<span class="font-weight-bold text-dark">Rating:</span>')))
     remarks = models.TextField(max_length=200, default= None, verbose_name=_(mark_safe('<span class="font-weight-bold text-dark">Description:</span>')))
-
-    
     
     def __str__(self):
         return self.rating
-    
-
-    
