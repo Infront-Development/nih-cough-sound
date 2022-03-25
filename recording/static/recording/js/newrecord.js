66a0f5eee958abe6c157ec2b3a278f2092fcbdb5
Jivha Raymond L <vharaymonten@gmail.com>
2022-03-25 13:35:51 +0700

    code cleanup

--- recording/static/recording/js/newrecord.js
+++ recording/static/recording/js/newrecord.js
@@ -13,10 +13,10 @@ const promptRecording = async (
   if (maskIndicator == "withMask" && methodIndicator == "cough") {
     Swal.fire({
       title: gettext(
-        "<div><img style='height: 120px;' src='../../../../static/img/Mask on.png' alt='Mask-on'/></div>" +
+        "<div><img style='height: 120px;' src='/static/img/Mask on.png' alt='Mask-on'/></div>" +
           "<div class='h5 text-white font-weight-bold'>Ensure you are in a quiet and safe environment <div style='color: #FF93DD;'>With Mask On</div>" +
           "<div><br>Example : </div>" +
-          '<div class="mt-2 mb-3"><audio controls><source src="../../../../static/audio/3. Cough Normal A.wav"><source src="../../../../static/audio/3. Cough Normal A.ogg"></audio></div>' +
+          '<div class="mt-2 mb-3"><audio controls><source src="/static/audio/3. Cough Normal A.wav"><source src="../../../../static/audio/3. Cough Normal A.ogg"></audio></div>' +
           "<div>"
       ),
       background: "#2B1392",
@@ -64,10 +64,10 @@ const promptRecording = async (
   } else if (maskIndicator == "noMask" && methodIndicator == "cough") {
     Swal.fire({
       title: gettext(
-        "<div><img style='height: 120px;' src='../../../../static/img/Mask off.png' alt='Mask-off'/></div>" +
+        "<div><img style='height: 120px;' src='/static/img/Mask off.png' alt='Mask-off'/></div>" +
       "<div class='h5 text-white font-weight-bold'>Ensure you are in a quiet safe environment before <div style='color: #FF93DD;'>removing your mask</div>" +
           "<div class='p-4'>Example:</div>" +
-          '<div class="mt-2 mb-3"><audio controls src="../../../../static/audio/3. Cough Normal A.wav"></audio></div>' +
+          '<div class="mt-2 mb-3"><audio controls src="/static/audio/3. Cough Normal A.wav"></audio></div>' +
           "<div>"
       ),
       background: "#2B1392",
