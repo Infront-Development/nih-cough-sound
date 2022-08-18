d5f91beb28dbfe164bd513b1bde2ac6454b8785c
Jivha <jivharaymond.lie@infrontconsulting.com.my>
2022-08-18 18:39:01 +0700

    create celery app

--- mohcough/settings.py
+++ mohcough/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'nxni+&g$@b@vyz)#i-z7-(($rsr27)6z%a8&a9x2nu@@j%fvll'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0',
                 'localhost', '20.212.39.73', 'cst-c19.southeastasia.cloudapp.azure.com', 'cough.infrontconsulting.asia', 'coughsound.dhri.my']
@@ -128,21 +128,27 @@ else:
         },
     }
 
-DATABASES = {
-'default': {
-        'ENGINE': 'mssql',
-        'HOST': os.environ['DBHOST'],
-        'PORT': '',
-        'NAME': os.environ['DBNAME'],
-        'USER': os.environ['DBUSER'], 
-        'PASSWORD': os.environ['DBPASSWORD'],
-        'OPTIONS': {
-            'driver': "ODBC Driver 17 for SQL Server",
-            'unicode_results': True,
-        },
-    },
-}
+# DATABASES = {
+# 'default': {
+#         'ENGINE': 'mssql',
+#         'HOST': os.environ['DBHOST'],
+#         'PORT': '',
+#         'NAME': os.environ['DBNAME'],
+#         'USER': os.environ['DBUSER'], 
+#         'PASSWORD': os.environ['DBPASSWORD'],
+#         'OPTIONS': {
+#             'driver': "ODBC Driver 17 for SQL Server",
+#             'unicode_results': True,
+#         },
+#     },
+# }
 print(DATABASES)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 AUTH_USER_MODEL = 'accounts.Account'
@@ -231,3 +237,8 @@ if not DEBUG:
     AZURE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=coughsoundadls;AccountKey=a3leb87q8xM1h42lp+iBwT69aWpdLLRvAWL/rbhl+7lvs6piivdwgbZWsm3Rpy4VytuDOTlKD6cuiDi7jqZ7Xg==;EndpointSuffix=core.windows.net"
     AZURE_CONTAINER = 'coughsoundproject' # Container or File System Name
     DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+
+
+# CELERY SETTINGS
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
