5fe2b083a969e715f05d80a9308556d49ce653cf
Laikaiyong <vandycklai@gmail.com>
2022-08-26 16:04:47 +0800

    Ignore async for now

--- mohcough/celery.py
+++
@@ -1,7 +0,0 @@
-import os 
-from celery import Celery
-
-app = Celery("mohcough")
-from . import settings
-app.config_from_object(settings, namespace='CELERY')
-app.autodiscover_tasks(['recording'])
