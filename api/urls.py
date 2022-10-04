6afa0180286149332cac114b711392f74c85f7c5
Jivha <jivharaymond.lie@infrontconsulting.com.my>
2022-10-04 13:18:18 +0700

    register url

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,10 @@ app_name = "api"
 
 from rest_framework.routers import DefaultRouter
 
-from .views import APIintegrationViewset
+from .views import APIintegrationViewset, AuthenticatiorViewset
 router = DefaultRouter()
 router.register(r'result', APIintegrationViewset, basename='result')
+router.register(r'token', AuthenticatiorViewset, basename='authentication')
 
 urlpatterns = [
 ]  +  router.urls
