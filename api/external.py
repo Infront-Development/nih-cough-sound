0a5d4a4f62666853240c44dfed4a0269ca34523f
Laikaiyong <vandycklai@gmail.com>
2022-10-14 16:43:17 +0800

    New upload endpoint url

--- api/external.py
+++ api/external.py
@@ -1,7 +1,7 @@
 import requests 
 
 UPLOAD_ENDPOINT_URL = "https://kxn3fbykyd.execute-api.ap-southeast-1.amazonaws.com/v1/coughsound/"
-NEW_UPLOAD_ENDPOINT_URL = "https://4tm1htfzw1.execute-api.ap-southeast-1.amazonaws.com/v1/"
+NEW_UPLOAD_ENDPOINT_URL = "https://4tm1htfzw1.execute-api.ap-southeast-1.amazonaws.com/v1/swinburne-cofe/"
 
 def send_to_aws(audio_buffer, filename):
     headers = {
