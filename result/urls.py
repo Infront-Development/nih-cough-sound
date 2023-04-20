fdd04a11ae3708b5f083213c92c340809556520f
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2023-04-20 15:23:42 +0700

    Contribute PAge

--- result/urls.py
+++ result/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.list_result, name="result_list"),
     path('analysis/', views.analyse_result, name="result_analysis"),
     path('history_result', views.history_result, name="history_result"),
-    path('update/status', views.edit_status, name="update_status")
+    path('update/status', views.edit_status, name="update_status"),
+    path('contribute', views.contribute_page, name="contribute"),
 ]
